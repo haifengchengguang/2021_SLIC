e4220301728af4ee355ab75a0abaf2df16e4ed8a
haifengchengguang <60420171+haifengchengguang@users.noreply.github.com>
2021-11-16 15:24:52 +0800

    3.51

--- SLIC.cpp
+++ SLIC.cpp
@@ -199,7 +199,7 @@ void SLIC::DetectLabEdges(
 	int sz = width*height;
 
 	edges.resize(sz,0);
-	//#pragma omp parallel for collapse(2) 
+	#pragma omp parallel for collapse(2) 
 	for( int j = 1; j < height-1; j++ )
 	{
 		for( int k = 1; k < width-1; k++ )
